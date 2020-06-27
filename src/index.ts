f990fab72aa59f3b50d0b7f4eefe76f8535f015b
Grimmer <grimmer0125@gmail.com>
2020-06-27 14:53:25 +0800

    Add the implement to read DICOM file name to ask chrome to show
    Use chrome extension,
    https://chrome.google.com/webstore/detail/dicom-image-viewer/ehppmcooahfnlfhhcflpkcjmonkoindc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,59 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+const shell = require('shelljs');
+const os = require('os');
+var path = require('path');
+
+// TODO1:
+// 1. [done] read single file, with file name parameter
+// 2. global npm package
+// TODO2:
+// 2. read current folder without any parameters
+// 3. read folder parameter, "./"" or ${some folder}
+// 4. read multiple files, a_file b_file
+
+// node index.ts fileName, or yarn dev fileName
+console.log(process.argv);
+let filePath = '';
+if (process.argv.length > 2) {
+  filePath = process.argv[2];
+  filePath = path.resolve(filePath); // process.cwd(). process path
+  console.log(filePath);
+
+  // if (fileName.indexOf('/') === -1) {
+  //   // TODO: no file path included, should add
+  //   console.log(__dirname); // script path, https://flaviocopes.com/node-get-current-folder/
+  //   fileName = `${__dirname}/${fileName}`;
+  // }
+}
+
+const extensionURL =
+  'chrome-extension://ehppmcooahfnlfhhcflpkcjmonkoindc/index.html';
+let fileURL = '';
+if (filePath) {
+  fileURL = `file://${filePath}`;
+}
+const chromeURL = `${extensionURL}#${fileURL}`;
+
+const platform = os.platform();
+if (platform === 'darwin') {
+  const command = `open -n -a "Google Chrome" --args  "${chromeURL}"`;
+  // Run external tool synchronously
+  if (shell.exec(command).code !== 0) {
+    shell.echo('Error: Launch Chrome failed');
+    shell.exit(1);
   }
-  return a + b;
-};
+} else if (platform === 'linux') {
+  let command = '';
+  if (shell.which('google-chrome')) {
+    command = `google-chrome "${chromeURL}"`;
+  } else if (shell.which('chromium-browser')) {
+    command = `chromium-browser "${chromeURL}"`;
+  }
+
+  // Run external tool synchronously
+  if (shell.exec(command).code !== 0) {
+    shell.echo('Error: Launch Chrome failed');
+    shell.exit(1);
+  }
+} else {
+  // Not implement yet "win32"
+}
