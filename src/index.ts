cc7bd610a606540509de3262d0da015005d6ae1d
Grimmer <grimmer0125@gmail.com>
2020-07-01 23:52:02 +0800

    Add try-catch when file not exist and ability to read multiple file name

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,41 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 
-// TODO1:
-// 1. [done] read single file, with file name parameter
-// 2. [done] global npm package
-// 3. [done] use "dicom" to open chrome extension
-// TODO2:
-// 1. done] read folder parameter, "./"" or ${some folder}
-// 2. (optional) read multiple files, a_file b_file
-
 function getFilePath() {
-  // node index.ts fileName, or yarn dev fileName, or dicom fileName?
+  // node index.ts fileName, or yarn dev fileName, or node dicom fileName
   if (process.argv.length > 2) {
-    const fileOrFolderPath = process.argv[2];
-    const absPath = path.resolve(fileOrFolderPath); // process.cwd(). process path
-
     let fileURL = '';
-    const stats = fs.lstatSync(absPath);
-    if (stats.isDirectory()) {
-      const files = fs.readdirSync(absPath);
-      files.forEach((file: string) => {
-        const fileAbsPath = `${absPath}/${file}`;
-        if (!fs.lstatSync(fileAbsPath).isDirectory()) {
+
+    process.argv.forEach((fileOrFolderPath, index) => {
+      console.log('i:', index, fileOrFolderPath);
+      if (index < 2) {
+        return;
+      }
+      const absPath = path.resolve(fileOrFolderPath); // process.cwd(). process path
+
+      try {
+        const stats = fs.lstatSync(absPath);
+        if (stats.isDirectory()) {
+          const files = fs.readdirSync(absPath);
+          files.forEach((file: string) => {
+            const fileAbsPath = `${absPath}/${file}`;
+            if (!fs.lstatSync(fileAbsPath).isDirectory()) {
+              if (!fileURL) {
+                fileURL = '#';
+              }
+              fileURL += `file://${fileAbsPath}`;
+            }
+          });
+        } else {
           if (!fileURL) {
             fileURL = '#';
           }
-          fileURL += `file://${fileAbsPath}`;
+          fileURL += `file://${absPath}`;
         }
-      });
-    } else {
-      fileURL += `#file://${absPath}`;
-    }
+      } catch (e) {
+        console.log('e:', e);
+      }
+    });
 
     return fileURL;
   }
